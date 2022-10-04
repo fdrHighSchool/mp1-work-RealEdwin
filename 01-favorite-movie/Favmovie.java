93d41f8b730fe89a1bd2f2b505a86e1908ed6bb5
AP Java Period 1 <apjavaperiod1@AC1MWK0LBJC9H.local>
2022-10-04 08:33:28 -0400

    updated favmovie 2

---
+++ 01-favorite-movie/Favmovie.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Favmovie here.
+ *
+ * @Edwin Velazquez
+ * @ 9/21/22
+ */
+public class Favmovie
+{
+    public static void main(String[] args) {
+    favoritemovie();
+  }//end of main method
+
+  public static void favoritemovie(){
+    System.out.println("My favorite movie is the Joker(2019).");
+    System.out.println("One reason that it's my favorite movie is that we see how others view society.");
+    System.out.println("Aother reason is that it shows how easy it can be for someone to cause a riot.");
+    System.out.println("The Joker shows us how important it is for people to have a good mental health.");
+    System.out.println("Joaquin Pheonix's performance was also why I think that the Joker is my favorite movie, his acting was amazing!");
+  }
+}
+
